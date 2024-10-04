8990dc2b036dbcbce61774b17f3d20a57537e082
Kauã Santos <89539085+KauaSanto1@users.noreply.github.com>
2024-10-04 15:13:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Detectando a rolagem e alterando a cor de fundo
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
     const documentHeight = document.body.scrollHeight - window.innerHeight;
     const scrollPercentage = (scrollPosition / documentHeight) * 100;
 
-    // Mudança de cor suave conforme a porcentagem de rolagem
     if (scrollPercentage < 50) {
         document.body.style.backgroundColor = '#433878'; // Cor inicial
     } else if (scrollPercentage >= 50) {
